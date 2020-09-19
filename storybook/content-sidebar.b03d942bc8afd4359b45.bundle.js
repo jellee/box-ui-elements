9a68c137c26de1820762b4d9b663bae75c8fdbd1
box-jenkins <automation.ext@box.com>
2020-09-19 01:50:10 +0000

    build(examples): v12.1.0-beta.1

---
+++ storybook/content-sidebar.b03d942bc8afd4359b45.bundle.js
@@ -0,0 +1,3 @@
+/*! For license information please see content-sidebar.b03d942bc8afd4359b45.bundle.js.LICENSE */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{4283:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var _ContentSidebar__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(1401);__webpack_require__.d(__webpack_exports__,"default",(function(){return _ContentSidebar__WEBPACK_IMPORTED_MODULE_0__.a}))}}]);
+//# sourceMappingURL=content-sidebar.b03d942bc8afd4359b45.bundle.js.map
